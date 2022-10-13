a2ef84f96b39b07718a8c71444e0dd2ad9fa8d7f
FlorencioQuelca <florens.lonwe@gmail.com>
2022-10-12 20:55:25 -0400

    actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useDispatch, useSelector } from 'react-redux'
+import {increment,decrement ,setCounterGlobal, reset} from './store/slices/counter.slice'
 import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
+  const counter =useSelector(state => state.counter)
+     const dispatch =useDispatch()
+   const handlePlus =() =>{
+    dispatch(increment())
+   }
+   const handleMinus =()=>{
+    dispatch(decrement())
+   }
+   const handleReset=()=>{
+    dispatch(reset())
+   }
+   
+    const  handleSet = () =>{
+    dispatch(setCounterGlobal("golas"))
+   }
   return (
     <div className="App">
-       <h1>redux</h1>
+       <h1>{counter}</h1>
+       <button onClick={handleSet} >set value too 100</button>
+       <button onClick={handlePlus} >+</button>
+       <button onClick={handleMinus} >-</button>
+       <button onClick={handleReset} >reset</button>
     </div>
   )
 }
--- src/store/slices/counter.slice.js
+++ src/store/slices/counter.slice.js
@@ -4,7 +4,11 @@ const counterSlice = createSlice({
     name: 'counter',
     initialState: 0,
     reducers: {
-
+        reset: () => 0,
+        decrement: state => state - 1,
+        increment: state => state + 1,
+        setCounterGlobal: (state, action) => action.payload
     }
 })
+export const { reset, decrement, increment, setCounterGlobal } = counterSlice.actions;
 export default counterSlice.reducer;
